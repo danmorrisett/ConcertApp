d64c81adaa3d41e882c750b37be7693101014462
Danmorrisett <danmorrisett@gMAIL.com>
2015-04-07 16:35:15 -0600

    Updated layouts

--- app/models/api.rb
+++ app/models/api.rb
@@ -6,7 +6,7 @@ class Api
 
   def shows(token)
     response = @conn.get do |req|
-      req.url "/events?artistId=2698&zipCode=95128&radius=50&page=0&api_key="
+      req.url "/events?artistId=2698&zipCode=95128&radius=50&page=0&api_key=6cxknuxyrakr8jwe8zcq8sjf"
       req.headers['Content-Type'] = 'application/json'
     end
 
@@ -18,11 +18,3 @@ class Api
     JSON.parse(response.body, symbolize_names: true)
   end
 end
-
-Content-Type: text/xml
-Date: Tue, 07 Apr 2015 17:44:19 GMT
-Server: Mashery Proxy
-X-Mashery-Error-Code: ERR_403_DEVELOPER_INACTIVE
-X-Mashery-Responder: prod-j-worker-us-east-1d-60.mashery.com
-Content-Length: 27
-Connection: keep-alive
